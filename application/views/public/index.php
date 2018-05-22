39c20ded98e64bc3c7ac95ad31d2e5c272fdb522
Martin <martinmatias@outlook.com>
2018-05-21 22:24:35 -0300

    Initial commit

---
+++ application/views/public/index.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Consultorio Médico</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Consultorio Médico</h1>
+
+	<div id="body">
+		<p>Proyecto iniciado</p>
+	</div>
+
+</div>
+
+</body>
+</html>
