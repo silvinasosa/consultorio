c868310df5ae32833c89df6ccaf80b20dee30609
Silvina Sosa <silvinass@MacBook-Pro-de-Silvina.local>
2018-05-22 22:39:52 -0300

    primer commit

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/consultio/';
+$config['base_url'] = 'http://localhost/consultorio/';
 
 $config['index_page'] = 'index.php';
 
